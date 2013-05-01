68df9b152954cfbf672d14fd91c8eaba1be3d309
idontlikemonday <dmitry.gorbunkov@harrycodeman.com>
2013-05-01 21:23:40 +0700

    AP-24 При добавлении тура, создавать поле, по которому в дальнейшем будет производиться поиск.

--- ap_post_types/ap_tour.php
+++ ap_post_types/ap_tour.php
@@ -20,3 +20,32 @@ function create_post_type() {
         )
     );
 }
+
+/*--- Adding Meta Box for Tour post type ---*/
+add_action( 'add_meta_boxes', 'add_tours_meta_boxes' );
+function add_tours_meta_boxes() {  
+    add_meta_box('truediv', 'Название', 'print_tour_meta_box', 'ap_tour', 'normal', 'high'); 
+}
+function print_tour_meta_box($post) {
+	wp_nonce_field( basename( __FILE__ ), 'seo_metabox_nonce' );
+	$html .= '<label>Поле для поиска тура <input type="text" name="seotitle" value="' . get_post_meta($post->ID, 'seo_title',true) . '" /></label> ';
+	echo $html;
+}
+add_action('save_post', 'save_tour_meta_box');
+function save_tour_meta_box ( $post_id ) {
+	if ( !isset( $_POST['seo_metabox_nonce'] )
+	|| !wp_verify_nonce( $_POST['seo_metabox_nonce'], basename( __FILE__ ) ) )
+        return $post_id;
+
+	if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) 
+		return $post_id;
+
+	if ( !current_user_can( 'edit_post', $post_id ) )
+		return $post_id;
+
+	$post = get_post($post_id);
+	if ($post->post_type == 'ap_tour') {
+		update_post_meta($post_id, 'seo_title', esc_attr($_POST['seotitle']));
+	}
+	return $post_id;
+}
