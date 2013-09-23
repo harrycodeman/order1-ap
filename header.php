167fb74418d05eb1c6135658ee23af7dc4bf083e
Zed Roth <littlepantry@gmail.com>
2013-09-23 21:03:37 +0700

    ap-82 (Доработка недостающих разделов сайта) добавлена поддержка Google Analytics.

--- header.php
+++ header.php
@@ -204,6 +204,17 @@ $(document).ready(function() {
 </head>
 
 <body <?php body_class(); ?>>
+<!-- Google Analytics code -->
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-44262122-1', 'alyeparusa.info');
+    ga('send', 'pageview');
+</script>
+
 <a id="top"></a>
 <div class="wrapper">
   <div class="header-wrapper">
