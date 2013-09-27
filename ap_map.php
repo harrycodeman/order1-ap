3aa6f7ae71508e89f488b5d1cd999d881dcc52d2
Zed Roth <littlepantry@gmail.com>
2013-09-28 01:34:37 +0700

    ap-77 (Раздел "Гид") добавлены новые иконки.

--- ap_map.php
+++ ap_map.php
@@ -8,19 +8,33 @@ get_header( ); ?>
 <div id="map" style="width: 100%;"></div>
 <div id="legend" style="background: #B72537; height: 30px;">
     <div class="clearfix" style="margin: 0 auto; width: 240px; color: #ffffff; font-size: 14px;">
-        <img src="<?php ap_print_image_url( 'map-icon-article.png' ); ?>" style=" float: left; padding: 7px;">
+        <img src="<?php ap_print_image_url( 'map/article-icon.png' ); ?>" style=" float: left; padding: 7px;">
         <div style="float: left; padding: 3px 20px 0 0; line-height: 25px;">Статьи</div>
-        <img src="<?php ap_print_image_url( 'map-icon-tour.png' ); ?>" style=" float: left; padding: 7px;">
+        <img src="<?php ap_print_image_url( 'map/tour-icon.png' ); ?>" style=" float: left; padding: 7px;">
         <div style="float: left; padding: 3px 20px 0 0; line-height: 25px;">Вылеты/Туры</div>
     </div>
 </div>
 <script>
     $(document).ready(function() {
         var styles = [{
-            url: '<?php ap_print_image_url( 'map/cluster-icon.png' ); ?>',
-            height: 50,
-            width: 55,
-            anchor: [1, 35],
+            url: '<?php ap_print_image_url( 'map/cluster-tours.png' ); ?>',
+            height: 49,
+            width: 49,
+            anchor: [12, 28],
+            textColor: '#ffffff',
+            textSize: 13
+        }, {
+            url: '<?php ap_print_image_url( 'map/cluster-articles.png' ); ?>',
+            height: 49,
+            width: 49,
+            anchor: [12, 28],
+            textColor: '#ffffff',
+            textSize: 13
+        }, {
+            url: '<?php ap_print_image_url( 'map/cluster-all.png' ); ?>',
+            height: 70,
+            width: 70,
+            anchor: [11, 40],
             textColor: '#ffffff',
             textSize: 13
         }];
@@ -42,10 +56,7 @@ get_header( ); ?>
         var map = new google.maps.Map(document.getElementById("map"), mapSettings);
         var searchService = new google.maps.places.PlacesService(map);
         var clusterer = new MarkerClusterer(map, [], { averageCenter: true, styles: styles });
-        var calculator = clusterer.getCalculator();
         clusterer.setCalculator(function (markers, numStyles) {
-            var result = calculator(markers, numStyles);
-
             var articlesCount = 0;
             var toursCount = 0;
             for (var i in markers) {
@@ -56,9 +67,23 @@ get_header( ); ?>
                     toursCount++;
                 }
             }
-            result.text = '<p style="margin-top: 5px;">' + toursCount + '</p><p style="margin-top: -8px;">' + articlesCount + '</p>';
 
-            return result;
+            if (articlesCount == 0) {
+                return {
+                    text: '<p style="margin-top: 5px;">' + toursCount + '</p>',
+                    index: 1
+                };
+            }
+            if (toursCount == 0) {
+                return {
+                    text: '<p style="margin-top: 4px;">' + articlesCount + '</p>',
+                    index: 2
+                };
+            }
+            return {
+                text: '<p style="margin-top: 5px;">' + articlesCount  + '</p><p style="margin-top: -4px;">' + toursCount + '</p>',
+                index: 3
+            };
         });
 
         <?php
@@ -80,7 +105,7 @@ get_header( ); ?>
                 if (status == google.maps.places.PlacesServiceStatus.OK && results.length >= 1) {
                     var place = results[0].geometry.location;
                     var marker = new google.maps.Marker({
-                        icon: '<?php ap_print_image_url('map-pin-article.png'); ?>',
+                        icon: '<?php ap_print_image_url('map/article.png'); ?>',
                         position: place,
                         map: map,
                         type: 'article'
@@ -117,7 +142,7 @@ get_header( ); ?>
                 if (status == google.maps.places.PlacesServiceStatus.OK && results.length >= 1) {
                     var place = results[0].geometry.location;
                     var marker = new google.maps.Marker({
-                        icon: '<?php ap_print_image_url('map-pin-tour.png'); ?>',
+                        icon: '<?php ap_print_image_url('map/tour.png'); ?>',
                         position: place,
                         map: map,
                         type: 'tour'
